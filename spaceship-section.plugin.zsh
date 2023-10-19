f6fd5b309d02027cac2d7d8922b81b01fee517ff
Harry Chen <40137928+hellocym@users.noreply.github.com>
2023-10-19 21:02:19 +0800

    Update spaceship-section.plugin.zsh

--- spaceship-section.plugin.zsh
+++ spaceship-section.plugin.zsh
@@ -1,55 +1,37 @@
 #
-# Foobar
+# CUDA
 #
-# Foobar is a supa-dupa cool tool for making you development easier.
+# Showing CUDA version.
 # Link: https://www.foobar.xyz
 
 # ------------------------------------------------------------------------------
 # Configuration
 # ------------------------------------------------------------------------------
 
-SPACESHIP_FOOBAR_SHOW="${SPACESHIP_FOOBAR_SHOW=true}"
-SPACESHIP_FOOBAR_ASYNC="${SPACESHIP_FOOBAR_ASYNC=true}"
-SPACESHIP_FOOBAR_PREFIX="${SPACESHIP_FOOBAR_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"}"
-SPACESHIP_FOOBAR_SUFFIX="${SPACESHIP_FOOBAR_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
-SPACESHIP_FOOBAR_SYMBOL="${SPACESHIP_FOOBAR_SYMBOL="😍 "}"
-SPACESHIP_FOOBAR_COLOR="${SPACESHIP_FOOBAR_COLOR="yellow"}"
+SPACESHIP_CUDA_SHOW=true
+SPACESHIP_CUDA_PREFIX=" "
+SPACESHIP_CUDA_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"
+SPACESHIP_CUDA_SYMBOL="|̲̅̅✇|̲̅̅=̲̅̅|̲̅̅✇|"
+SPACESHIP_CUDA_COLOR="green"
 
 # ------------------------------------------------------------------------------
 # Section
 # ------------------------------------------------------------------------------
 
-# Show foobar status
+# Show CUDA status
 # spaceship_ prefix before section's name is required!
 # Otherwise this section won't be loaded.
-spaceship_foobar() {
-  # If SPACESHIP_FOOBAR_SHOW is false, don't show foobar section
-  [[ $SPACESHIP_FOOBAR_SHOW == false ]] && return
+spaceship_cuda() {
+  [[ $SPACESHIP_CUDA_SHOW == false ]] && return
 
-  # Check if foobar command is available for execution
-  spaceship::exists foobar || return
+  local cuda_version="cu$(nvcc --version | grep release | awk '{print $6}' | cut -c2-)"
 
-  # Show foobar section only when there are foobar-specific files in current
-  # working directory.
+  [[ -z "$cuda_version" ]] && return
 
-  # spaceship::upsearch utility helps finding files up in the directory tree.
-  local is_foobar_context="$(spaceship::upsearch foobar.conf)"
-  # Here glob qualifiers are used to check if files with specific extension are
-  # present in directory. Read more about them here:
-  # http://zsh.sourceforge.net/Doc/Release/Expansion.html
-  [[ -n "$is_foobar_context" || -n *.foo(#qN^/) || -n *.bar(#qN^/) ]] || return
-
-  local foobar_version="$(foobar --version)"
-
-  # Check if tool version is correct
-  [[ $foobar_version == "system" ]] && return
-
-  # Display foobar section
-  # spaceship::section utility composes sections. Flags are optional
-  spaceship::section::v4 \
-    --color "$SPACESHIP_FOOBAR_COLOR" \
-    --prefix "$SPACESHIP_FOOBAR_PREFIX" \
-    --suffix "$SPACESHIP_FOOBAR_SUFFIX" \
-    --symbol "$SPACESHIP_FOOBAR_SYMBOL" \
-    "$foobar_version"
+  spaceship::section \
+    --color "$SPACESHIP_CUDA_COLOR" \
+    --prefix "$SPACESHIP_CUDA_PREFIX" \
+    --suffix "$SPACESHIP_CUDA_SUFFIX" \
+    --symbol "$SPACESHIP_CUDA_SYMBOL" \
+    "$cuda_version"
 }
